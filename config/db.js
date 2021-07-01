3c4222fca21ba77b763ada6911f5c2d00e21768e
Mohit Mayank <65847302+mohit98350@users.noreply.github.com>
2021-07-01 21:24:21 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mongoose  =  require("mongoose")
 require('dotenv').config()
 
+const DB='mongodb+srv://Mohit:mohit@cluster0.45yts.mongodb.net/mernstack?retryWrites=true&w=majority'
 module.exports =  connect = async ()=>{
     try{
-        const response = await mongoose.connect(process.env.URL,{useNewUrlParser:true,useUnifiedTopology:true});
+        const response = await mongoose.connect(DB,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false});
         console.log("connection created..")
     }catch(error){
         console.log(error)
